305c1c9720e9dc3ee5a1e352c15dd849899b441e
dzil123 <5725958+dzil123@users.noreply.github.com>
2019-12-29 21:44:31 -0800

    NavX invert constant

--- src/main/java/frc/robot/util/NavX.java
+++ src/main/java/frc/robot/util/NavX.java
@@ -8,13 +8,19 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 public class NavX {
     private NavX() {}
 
-    public static AHRS ahrs = new AHRS(SPI.Port.kMXP, (byte)100);
+    public static final byte updateRate = 100;
+    public static final boolean invert = false; // TODO: ensure left turn is positive angle
+
+    public static AHRS ahrs = new AHRS(SPI.Port.kMXP, updateRate);
+    public static final double invertConst = invert ? -1 : 1;
 
     // getYaw() -180 - 180
     // getAngle() continuous
-    // TODO: ensure left turn is positive angle, probably need negative
-
     public static Rotation2d getRotation() {
-        return Rotation2d.fromDegrees(ahrs.getYaw());
+        return Rotation2d.fromDegrees(ahrs.getYaw() * invertConst);
+    }
+
+    public static double getRate() {
+        return ahrs.getRate() * invertConst;
     }
 }
