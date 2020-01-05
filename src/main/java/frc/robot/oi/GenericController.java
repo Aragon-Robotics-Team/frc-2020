42d69fc1d822313c63ea2c9546193acfa7d5be13
Daniel Z <5725958+dzil123@users.noreply.github.com>
2020-01-05 00:32:55 -0800

    Add Button and Axis mapping for GenericController
    Co-Authored-By: tomdabomb2197 <tomdabomb2197@users.noreply.github.com>
    Co-Authored-By: Joseph <cobalt396@users.noreply.github.com>

--- src/main/java/frc/robot/oi/GenericController.java
+++ src/main/java/frc/robot/oi/GenericController.java
@@ -9,9 +9,21 @@ public class GenericController extends Joystick {
         public static final int B = 2;
         public static final int X = 3;
         public static final int Y = 4;
+        public static final int LBump = 5;
+        public static final int RBump = 6;
+        public static final int MiddleL = 7;
+        public static final int MiddleR = 8;
+        public static final int LStickDown = 9;
+        public static final int RStickDown = 10;
     }
 
     private static final class Axis {
+        public static final int LX = 0;
+        public static final int LY = 1;
+        public static final int LT = 2;
+        public static final int RT = 3;
+        public static final int RX = 4;
+        public static final int RY = 5;
     }
 
     public GenericController(final int port) {
@@ -19,19 +31,19 @@ public class GenericController extends Joystick {
     }
 
     public final double getThrottle() {
-        return -getAxis(0);
+        return -getAxis(Axis.LY);
     }
 
     public final double getTurn() {
-        return getAxis(0);
+        return getAxis(Axis.RX);
     }
 
     public final double getThrottle2() {
-        return -getAxis(0);
+        return -getAxis(Axis.RY);
     }
 
     public final double getTurn2() {
-        return getAxis(0);
+        return getAxis(Axis.LX);
     }
 
     public boolean disableCompressor() {
